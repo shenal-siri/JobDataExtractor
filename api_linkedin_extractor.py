f747556345131dc821a28b44a0f85c91178b0052
shenal-siri <shenal.siri@gmail.com>
2020-07-21 07:56:14 -0400

    Initial integration of html_processor.py to API

--- api_linkedin_extractor.py
+++ api_linkedin_extractor.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 from flask_restful import Api, Resource, reqparse, fields, marshal
+from html_processor import JobData
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,19 +8,31 @@ api = Api(app)
 # Define the fields to be yielded during GET requests
 job_fields = {
     'id': fields.Integer,
-    'HTML': fields.String,
+    'url': fields.String,
+    'title': fields.String,
+    'company': fields.String,
+    'location': fields.String,
+    'seniority': fields.String,
+    'employment_type': fields.String,
+    'industries': fields.List(fields.String),
+    'functions': fields.List(fields.String),
+    'posting_text': fields.String,
     'uri': fields.Url('job')
 }
 
-# Stand-in list of 'job' objects to test API (pending database implementation)
+# Stand-in data store of 'job' objects to test API (pending database implementation)
 jobs = [
     {
         'id': 1,
-        'HTML': 'THIS IS THE HTML FOR JOB WITH ID 1'
-    },
-    {
-        'id': 2,
-        'HTML': 'JOB #2 IS REALLY COOL, APPLY NOW!!!'
+        'url': "test url",
+        'title': "test title",
+        'company': "test company",
+        'location': "test location",
+        'seniority': "test seniority",
+        'employment_type': "test employment type",
+        'industries': ["test industry 1", "test industry 2"],
+        'functions': ["test function 1", "test function 2", "test function 3"],
+        'posting_text': "test job posting text, description and information"
     }
 ]
 
@@ -38,14 +51,20 @@ class JobListAPI(Resource):
         super(JobListAPI, self).__init__()
 
     def post(self):
-        # Assign the id and HTML received from the Chrome Extension into a bundled 'current_job'
+        # Assign the id and HTML received from the Chrome Extension into a ata object
         args = self.reqparse.parse_args()
-        current_job = {
+        job_args = {
             'id': args['id'],
-            'HTML': args['HTML']
+            'html': args['HTML']
         }
-        jobs.append(current_job)
-        return {'job': marshal(current_job, job_fields)}, 201
+        current_job = JobData(job_posting_data=job_args)
+        
+        # Have the JobData object extract the relevant data fields from the raw HTML
+        current_job.extract_job_data()
+        
+        # Add the JobData object data to our temporary data store
+        jobs.append(current_job.data)
+        return {'job': marshal(current_job.data, job_fields)}, 201
 
 
 class JobAPI(Resource):
